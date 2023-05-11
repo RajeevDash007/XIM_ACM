4c2b23ca0d9cff8a3162a693e62d4dbb020a2a82
Nimoo2003 <ucse21044@stu.xim.edu.in>
2023-05-11 12:49:43 +0530

    updated the form by adding php file

---
+++ EventDetails/events.php
@@ -0,0 +1,26 @@
+<?php
+
+    $connection = mysqli_connect('localhost','root','','events_db');
+
+    if(isset($_POST['send'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $nos = $_POST['nos'];
+        $college = $_POST['college']; 
+    
+    
+
+    $request="INSERT into register_form(name , email , number , college) VALUES ('$name','$email','$nos','$college')";
+
+    $result = mysqli_query($connection,$request);
+
+    mysqli_close($connection);
+
+    header('location:Technical_Writing.html');
+    }else{
+        echo 'something went wrong try again'; 
+    }
+
+
+
+?>
